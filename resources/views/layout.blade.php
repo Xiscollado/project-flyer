c0c1c990a718274c36ba0a76d55d4c88406cd146
Xisco Llado <x.llado@hotelinking.com>
2016-05-23 15:38:54 +0200

    flyers.create page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,26 @@
 	<link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
+	<nav class="navbar navbar-inverse" style="border-radius:0">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Brand</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<div class="container">
 		@yield('content')
 	</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-	<h1>Project Flyer</h1>
+<div class="jumbotron">
+	<div class="container">
+		<h1>Hello, world!</h1>
+		<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+		<p><a class="btn btn-primary btn-lg" href="/flyers/create" role="button">Create a Flyer »</a></p>
+	</div>
+</div>
 
 @stop
